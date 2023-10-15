7713544aac40f0986c2e18a005f82cded0f2e2a8
Johannes Becker <git.acct02@outlook.com>
2023-10-15 14:30:00 +0800

    feat(config): add xs breakpoint for better responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
     ],
     theme: {
         extend: {
+            screens: {
+                xs: "480px", // Added extra small breakpoint
+            },
             spacing: {
                 '128': '32rem',
                 '144': '36rem',
