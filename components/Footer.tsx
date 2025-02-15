86a6aef24992275678d682fb5f2b75df1aa35d2a
Johannes Becker <git.acct02@outlook.com>
2025-02-15 21:26:43 +0800

    feat(footer): added social media links and improved layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,12 @@
 export default function Footer() {
-    return (
-      <footer className="bg-gray-800 text-white text-center p-4 mt-10">
-        <p>&copy; 2025 AETHERSCAN. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
+  return (
+    <footer className="bg-gray-800 text-white text-center p-4 mt-10">
+      <p>&copy; 2025 AETHERSCAN. All rights reserved.</p>
+      <div className="mt-2">
+        <a href="https://twitter.com/AetherscanAI" className="mx-2 text-blue-400 hover:underline">Twitter</a> |
+        <a href="https://discord.gg/samplelink" className="mx-2 text-blue-400 hover:underline">Discord</a> |
+        <a href="https://github.com/Aetherscan" className="mx-2 text-blue-400 hover:underline">GitHub</a>
+      </div>
+    </footer>
+  );
+}
