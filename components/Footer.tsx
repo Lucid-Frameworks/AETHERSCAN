fc8681aa0f28c3f87d482987a85479cddfac9c3a
Johannes Becker <git.acct02@outlook.com>
2025-04-06 12:00:00 +0800

    chore(Footer.tsx): add additional social links and improve styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,9 +5,10 @@ export default function Footer() {
       <div className="mt-2">
         <a href="https://twitter.com/AetherscanAI" className="mx-2 text-blue-400 hover:underline">Twitter</a> |
         <a href="https://discord.gg/samplelink" className="mx-2 text-blue-400 hover:underline">Discord</a> |
-        <a href="https://github.com/Aetherscan" className="mx-2 text-blue-400 hover:underline">GitHub</a>
+        <a href="https://github.com/Aetherscan" className="mx-2 text-blue-400 hover:underline">GitHub</a> |
+        <a href="https://www.linkedin.com/company/aetherscan" className="mx-2 text-blue-400 hover:underline">LinkedIn</a> |
+        <a href="mailto:contact@aetherscan.io" className="mx-2 text-blue-400 hover:underline">Email</a>
       </div>
     </footer>
   );
 }
-  
