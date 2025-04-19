aff97307221119abbdddb48a24aee7aa311885d8
Johannes Becker <git.acct02@outlook.com>
2025-04-19 12:00:00 +0800

    feat(Footer.tsx): add a copyright link and refine layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white text-center p-4 mt-10">
-      <p>&copy; 2025 AETHERSCAN. All rights reserved.</p>
+      <p>&copy; 2025 AETHERSCAN. All rights reserved. <Link href="/privacy-policy"><a className="text-blue-400 hover:underline">Privacy Policy</a></Link></p>
       <div className="mt-2">
         <a href="https://twitter.com/AetherscanAI" className="mx-2 text-blue-400 hover:underline">Twitter</a> |
         <a href="https://discord.gg/samplelink" className="mx-2 text-blue-400 hover:underline">Discord</a> |
